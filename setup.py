e462e055225c2fa893665755a1fae5c4dab91abf
Rahul Bundekar <rahulbundekar@gmail.com>
2025-07-27 10:58:03 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Generative AI Project",
+    version = "0.0.1",
+    author = "Rahul",
+    author_email = "rahulbundekar1@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
